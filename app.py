13135921d0cf443288a0167490d59c57e8c2347a
Reza Ghasemi <rezaghasemi.cs@gmail.com>
2023-12-27 02:26:37 -0700

    Initialization

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import sklearn
+from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
+
+
+st.write('My First Project For Machine Leaning')
+
+st.button()
+
